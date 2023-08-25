ad00de6a8cda135b3bb693cd82b92d6a09799d19
leomaltta <leonardoomaltaa2@gmail.com>
2023-08-24 22:32:42 -0300

    updating

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
 function playSound(audioSelector) {
   const element = document.querySelector(audioSelector);
-    if (element && element.localName === 'audio') {
-        element.play();
-    } else {
-        console.log('Element Not found')
-    }
+  if (element && element.localName === "audio") {
+    element.play();
+  } else {
+    console.log("Element Not found");
+  }
 }
 
-const list = document.querySelectorAll('.tecla')
-
-
+const list = document.querySelectorAll(".tecla");
 
 for (let count = 0; count < list.length; count++) {
-    const key = list[count]
-    const instrument = key.classList[1]
+  const key = list[count];
+  const instrument = key.classList[1];
 
-    key.onclick = function () {
-        playSound(`#som_${instrument}`);
-    }
+  key.onclick = function () {
+    playSound(`#som_${instrument}`);
+  };
 
-    key.onkeydown = function(e) {
-        if (e.code === 'Space' || e.code === 'Enter') {
-        key.classList.add('ativa')
-        }
+  key.onkeydown = function (e) {
+    if (e.code === "Space" || e.code === "Enter") {
+      key.classList.add("ativa");
     }
-    key.onkeyup = function() {
-        key.classList.remove('ativa')
-    }
+  };
+  key.onkeyup = function () {
+    key.classList.remove("ativa");
+  };
 }
-    
